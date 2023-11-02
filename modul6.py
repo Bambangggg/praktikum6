264300dc09d307b6618f144c1a4237348cc915d5
Bambangggg <83066433+Bambangggg@users.noreply.github.com>
2023-11-02 19:54:02 +0700

    Add files via upload

---
+++ modul6.py
@@ -0,0 +1,9 @@
+def nilai_rata(a, b):
+    rata_rata = (a + b) / 2
+    return rata_rata
+
+
+
+nilai_pertama = int(input("masukkan nilai pertama : "))
+nilai_kedua = int(input ("masukkan Nilai kedua : "))
+print("rata - ratanya adalah :", nilai_rata(nilai_pertama, nilai_kedua))
